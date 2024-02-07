85e6a93315e422c471ea4f3d3a41a77fca143f26
MartinSSama07 <144184389+MartinSSama07@users.noreply.github.com>
2024-02-06 23:10:13 -0600

    api de conceptos

--- glosario_methods/app/Http/Controllers/ConceptosController.php
+++ glosario_methods/app/Http/Controllers/ConceptosController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\Conceptos;
 
 class ConceptosController extends Controller
 {
-    
+    public function register(Request $request)
+    {
+        $concept = new Conceptos();
+        $concept->name = $request->name;
+        $concept->concepto = $request->concepto;
+        $concept->referencia = $request->referencia;
+        $texto = $request->input('name');
+        $primeraLetra = strtoupper(Str::substr($texto,0,1));
+        $concept->indice=$primeraLetra;
+        $concept->save();
+        return response()->json(['message' => 'Concepto creado con exito :D goku le gana', 'estado' => 'ok']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $concept = Conceptos::findOrFail($id);
+        $concept->name = $request->name;
+        $concept->concepto = $request->concepto;
+        $concept->referencia = $request->referencia;
+        $concept->save();
+        return response()->json(['message' => 'Concepto actualizado con exito :D goku le gana', 'estado' => 'ok']);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $concept = Conceptos::findOrFail($id);
+            $concept->delete();
+
+            return response()->json(['message' => 'Concepto borrado exitosamente', 'estado' => 'ok'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al eliminar el concepto', 'estado' => 'error'], 500);
+        }
+    }
+
+
 }
